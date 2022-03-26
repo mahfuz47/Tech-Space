b573fa363fc59c2f6d8454eb00b8f034a873d1e5
mahfuz47 <mahfuzshikder560@gmail.com>
2022-03-26 08:38:43 +0600

    header codes solved

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-section">
+      <h1 className="header">TECH SPACE</h1>
+      <p>Buy your dream</p>
+    </div>
+  );
+};
+
+export default Header;
